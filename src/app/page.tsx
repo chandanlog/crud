3a1f8d92c889c82d47e825f2bc8da892053edb79
Chandan Kumar Mahto <65109892+chandanlog@users.noreply.github.com>
2025-03-29 23:41:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 import FormTask from "@/components/FormTask";
 import ShowTasks from "@/components/ShowTasks";
-import { useRouter, useParams} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
-  
   return (
-    <div className="bg-gray-100">
-      <div className="container mx-auto flex justify-between  ">
-        <div className="">
-          <FormTask></FormTask>
-        </div>
-        <div className="">
-          <ShowTasks></ShowTasks>
+    <div className="bg-gray-100 min-h-screen py-6">
+      <div className="container mx-auto px-4 md:px-8 lg:px-16">
+        <div className="flex flex-col md:flex-row gap-6">
+          {/* Form Section */}
+          <div className="w-full md:w-1/2">
+            <FormTask />
+          </div>
+
+          {/* Task Display Section */}
+          <div className="w-full md:w-1/2">
+            <ShowTasks />
+          </div>
         </div>
       </div>
     </div>
